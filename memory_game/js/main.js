a2db79950a091ea8f20027a46f25e2f44dbf747b
Rocky Liwanag <studiorockprod@me.com>
2020-01-19 09:44:57 -0800

    created objects for card array

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,11 +1,34 @@
-const cards = ['queen', 'queen', 'king', 'king'];
+const cards = [
+	{
+		rank: 'queen',
+		suit: 'hearts',
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: 'queen',
+		suit: 'diamonds',
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: 'king',
+		suit: 'hearts',
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: 'king',
+		suit: 'diamonds',
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
 let cardsInPlay = [];
 
 //function to store the steps to check for a match
 function checkForMatch(){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
+	  alert("You found a match!");
 	  console.log("You found a match!");
 	} else {
+		alert("Sorry, try again");
 	  console.log("Sorry, try again.");
 	}
 
@@ -15,8 +38,8 @@ function flipCard(cardId) {
 	if (cardsInPlay.length === 2) {
 	  checkForMatch();   
 	}
-	cardsInPlay.push(cards[cardId]);
-	console.log("User flipped " + cards[cardId]);	
+	cardsInPlay.push(cards[cardId].rank);
+	console.log("User flipped " + cards[cardId].rank, cards[cardId].cardImage);	
 }
 flipCard(0);
 flipCard(2);

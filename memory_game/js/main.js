bdf3311f5ec44265654545b93bc1373cc7cc3b91
Rocky Liwanag <studiorockprod@me.com>
2020-01-18 22:54:57 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,22 +1,27 @@
 const cards = ['queen', 'queen', 'king', 'king'];
 let cardsInPlay = [];
 
-let cardOne = cards[0];
-
-cardsInPlay.push('cardOne');
-
-console.log('User flipped ' + cardOne);
-
-let cardTwo = cards[1];
-
-cardsInPlay.push('cardTwo');
-
-console.log('User flipped ' + cardTwo);
-
-if (cardsInPlay.length === 2) {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert('You found a match!');
-} else {
-	alert('Sorry, try again!');
+//function to store the steps to check for a match
+function checkForMatch(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	  console.log("You found a match!");
+	} else {
+	  console.log("Sorry, try again.");
 	}
+
 };
+//function for user to flip cards
+function flipCard(cardId) {
+	if (cardsInPlay.length === 2) {
+	  checkForMatch();   
+	}
+	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId]);	
+}
+flipCard(0);
+flipCard(2);
+checkForMatch();
+
+
+
+

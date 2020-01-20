1d555717c4ce0e68eadf9b92e8a91650f1e277a0
Rocky Liwanag <studiorockprod@me.com>
2020-01-20 00:25:36 -0800

    set logic to flip cards

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -22,29 +22,37 @@ const cards = [
 ];
 let cardsInPlay = [];
 
+//create board function
+function createBoard(){
+	for (let i = 0; i < cards.length; i++) {
+		let cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', [i]);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementsByTagName('div')[0].appendChild(cardElement);
+	}
+};
+
+//function for user to flip cards
+function flipCard() {
+	let cardId = this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardId].cardImage);
+	if (cardsInPlay.length === 1) {
+	  checkForMatch();   
+	}
+	cardsInPlay.push(cards[cardId].rank);
+};
+
 //function to store the steps to check for a match
 function checkForMatch(){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 	  alert("You found a match!");
-	  console.log("You found a match!");
 	} else {
 		alert("Sorry, try again");
-	  console.log("Sorry, try again.");
 	}
 
 };
-//function for user to flip cards
-function flipCard(cardId) {
-	if (cardsInPlay.length === 2) {
-	  checkForMatch();   
-	}
-	cardsInPlay.push(cards[cardId].rank);
-	console.log("User flipped " + cards[cardId].rank, cards[cardId].cardImage);	
-}
-flipCard(0);
-flipCard(2);
-checkForMatch();
 
 
 
-
+createBoard();

f2e67a41e5ec35913af1a66addebcdb92a123449
Rocky Liwanag <studiorockprod@me.com>
2020-02-05 21:23:29 -0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# wdi-fundamentals-memorygame
+# wdi-fundamentals-memorygame# TIC-TAC-TOE
--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,4 @@
-const cards = [
+var cards = [
 	{
 		rank: 'queen',
 		suit: 'hearts',
@@ -20,29 +20,29 @@ const cards = [
 		cardImage: "images/king-of-diamonds.png"
 	}
 ];
-let cardsInPlay = [];
+var cardsInPlay = [];
 
 //create board function
 function createBoard(){
-	for (let i = 0; i < cards.length; i++) {
-		let cardElement = document.createElement('img');
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
 		cardElement.setAttribute('src', 'images/back.png');
 		cardElement.setAttribute('data-id', i);
 		cardElement.addEventListener('click', flipCard);
 		document.getElementById('game-board').appendChild(cardElement);
 	}
-};
+}
 
 //function for user to flip cards
 function flipCard() {
-	let cardId = this.getAttribute('data-id');//0
+	var cardId = this.getAttribute('data-id');//0
 	this.setAttribute('src', cards[cardId].cardImage);
 	cardsInPlay.push(cards[cardId].rank);
 
 	if (cardsInPlay.length === 2) {
 	  checkForMatch();   
 	}
-};
+}
 
 //function to store the steps to check for a match
 function checkForMatch(){
@@ -53,6 +53,6 @@ function checkForMatch(){
 	
 		}
 	
-};
+}
 
 createBoard();
